e29e6445b91e68d2041116cc81d0cfc98daca7d1
Durgesh Tambe <dntambe@gmail.com>
2026-09-05 17:14:27 +0530

    Add getCurrentUser endpoint to AuthController for fetching user details

--- api/src/main/java/com/hams/hospital_appointment_system/module/auth/controller/AuthController.java
+++ api/src/main/java/com/hams/hospital_appointment_system/module/auth/controller/AuthController.java
@@ -3,13 +3,19 @@ package com.hams.hospital_appointment_system.module.auth.controller;
 import com.hams.hospital_appointment_system.module.auth.dto.LoginRequest;
 import com.hams.hospital_appointment_system.module.auth.dto.LoginResponse;
 import com.hams.hospital_appointment_system.module.auth.service.AuthService;
+import com.hams.hospital_appointment_system.module.user.service.UserService;
+import com.hams.hospital_appointment_system.module.user.dto.UserResponse;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -17,10 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-        LoginResponse loginResponse  = authService.login(loginRequest);
+        LoginResponse loginResponse = authService.login(loginRequest);
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser(Authentication authentication) {
+        String username = authentication.getName();
+
+        return ResponseEntity.ok(userService.getUserByUsername(username));
     }
 }
