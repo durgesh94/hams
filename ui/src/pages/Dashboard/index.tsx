bbefd2382f0bbe914b4491628a491a77d0d794a1
Durgesh Tambe <dntambe@gmail.com>
2026-09-06 09:57:32 +0530

    added redux

--- ui/src/pages/Dashboard/index.tsx
+++ ui/src/pages/Dashboard/index.tsx
@@ -3,6 +3,15 @@ import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 const Dashboard = () => {
   return (
     <Box>
+      <Box sx={{ mb: 3, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          Dashboard
+        </Typography>
+        <Typography variant="h6" sx={{ mb: 3 }}>
+          {new Date().toLocaleString("default", { month: "long" })} {new Date().getFullYear()}
+        </Typography>
+      </Box>
+
       <Grid container spacing={3}>
         <Grid size={{ xs: 12, md: 4 }}>
           <Card>
